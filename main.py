f65400c211ef99bb4fe008994afb95ab7afffcc7
Nik Drummond <nikolasdrummond@gmail.com>
2021-01-27 16:47:49 +0100

    modularity

--- main.py
+++ main.py
@@ -34,30 +34,11 @@ def find_start_codon(rna_string):
 def get_codon_from_RNA(RNA_string):
     codon_list = []
     for i in range(0,len(RNA_string),3):
-        RNA_string[i:i+3]
+        codon_list.append(RNA_string[i:i+3])
+    amino_acid_list = []
+    for codon in codon_list:
+        amino_acid_list.append(codons_dict[codon])
+    return amino_acid_list
 
-cases_DNA_complement = [
-    ("ATG", "CAT"),
-    ("GGCCTA","TAGGCC")
-    ]
 
-cases_DNAtoRNA = [
-    ("ATG", "CAU"),
-    ("GGCCTA","UAGGCC")
-    ]
-cases_codon_from_RNA = [
-    ("AUG", "Met")
-    ("AUGGAA", ["Met", "Glu"])
-    ]
 
-@pytest.mark.parametrize(["string", "complement"], cases_DNA_complement)
-def test_make_complement(string,complement):
-    assert make_complement(string)==complement
-
-@pytest.mark.parametrize(["string", "complement"], cases_DNAtoRNA)
-def test_make_RNA(string,complement):
-    assert make_RNA(string)==complement
-
-@pytest.mark.parametrize(["string", "complement"], cases_codon_from_RNA)
-def test_get_codon(string,complement):
-    assert get_codon_from_RNA(string)== complement
---
+++ test.py
@@ -0,0 +1,28 @@
+from main import *
+
+
+cases = [
+    ("ATG", "CAT"),
+    ("GGCCTA","TAGGCC")
+    ]
+@pytest.mark.parametrize(["string", "complement"], cases)
+def test_make_complement(string,complement):
+    assert make_complement(string)==complement
+
+
+cases = [
+    ("ATG", "CAU"),
+    ("GGCCTA","UAGGCC")
+    ]
+@pytest.mark.parametrize(["string", "complement"], cases)
+def test_make_RNA(string,complement):
+    assert make_RNA(string)==complement
+
+
+cases = [
+    ("AUG", "Met")
+    ("AUGGAA", ["Met", "Glu"])
+    ]
+@pytest.mark.parametrize(["string", "complement"], cases)
+def test_get_codon(string,complement):
+    assert get_codon_from_RNA(string)== complement
