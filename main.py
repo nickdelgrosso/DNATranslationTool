7465fbfb5b68a3afcc8f9ecad043bfbf8a8c41f9
Nik Drummond <nikolasdrummond@gmail.com>
2021-01-27 14:14:03 +0100

    DNA complemet function v1

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 ## This python file will take a string of DNA letters and do something with it
 import pytest
 
-complement_strand= ""
+# create lookup DNA dict
+DNA_dict  = {"A":"T",
+            "T":"A",
+            "G":"C",
+            "C":"G"}
+
+
 def make_complement(string):
+    complement_strand= ""
     for element in string:
-        complement_strand.append(element)
+        complement_strand.join(DNA_dict[element])
+
+    return(complement_strand)
 
 
-cases = []
-    ("ATG",  )
-]
+cases = [
+    ("ATG", "TAC"),
+    ("GGCCTA","CCGGAT")
+    ]
 
 @pytest.mark.parametrize("string,complement", cases)
 def test_make_complement(string,complement):
