29e0f22586549ea696293886616e99a85b740e07
arne-fabritius <arne@sci-illustrate.com>
2021-01-27 14:04:52 +0100

    setting test

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 ## This python file will take a string of DNA letters and do something with it
+import pytest
 
 complement_strand= ""
-def turn_DNA_to_complement_strand(string):
+def make_complement(string):
     for element in string:
         complement_strand.append(element)
+
+
+cases = []
+    ("ATG",  )
+]
+
+@pytest.mark.parametrize("string,complement", cases)
+def test_make_complement(string,complement):
+    assert make_complement(string)==complement
