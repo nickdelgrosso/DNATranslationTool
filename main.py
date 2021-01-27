1f86f7bb6277bac3e341b7be23688fa602ea4b75
nhoermann <58261267+nhoermann@users.noreply.github.com>
2021-01-27 12:13:55 +0100

    Added first function to convert strand into complement strand.

---
+++ main.py
@@ -0,0 +1,6 @@
+## This python file will take a string of DNA letters and do something with it
+
+complement_strand= ""
+def turn_DNA_to_complement_strand(string):
+    for element in string:
+        complement_strand.append(element)
