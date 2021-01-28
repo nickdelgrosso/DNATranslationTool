01117e7be29e6152ca372f45610a75c4cb1ad413
hemesath <hemesath@neuro.mpg.de>
2021-01-28 16:54:32 +0100

    Complete validate DNA seq function
    >
    >
    Co-authored-by: Caroline Weiand
    <Weiand@neuro.mpg.de>
    Co-authored-by: Jonas Hemesath
    <jonas-hemesath@web.de>
    Co-authored-by: Matt McCann
    <matthew.mccann13@gmail.com>
    Co-authored-by: Nikolai Hoermann
    <hoermann@neuro.mpg.de>

--- validation.py
+++ validation.py
@@ -5,7 +5,3 @@ def validate_DNA_seq(DNA_string):
         if N not in DNA_dict.keys():
             raise ValueError("Wrong nucletide character")
 
-
-
-if __name__ == "__main__":
-    validate_DNA_seq('A')
